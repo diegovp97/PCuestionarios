de555feb2880b4b9d7ed02470e5562cbbee286ab
diegovp97 <122355831+diegovp97@users.noreply.github.com>
2024-05-16 10:27:06 +0200

    nuevo

--- src/app/models/survey.ts
+++ src/app/models/survey.ts
@@ -2,3 +2,14 @@ export interface Survey{
     titulo: string;
     subtitulo: string;
 }
+
+interface Item {
+    title: string;
+    type: string;
+  }
+
+interface Pregunta {
+    title: string;
+    subtitle: string;
+    items: Item[]; 
+  }
